e775e195fda8427438813fb94482b8a2419b37b3
agaslan <agaslanramazan27@gmail.com>
2024-05-14 15:50:52 +0300

    test yazmaya basladım. doktor icin test yazdım yazarken bug buldum duzelttim

---
+++ src/test/java/DoctorTest.java
@@ -0,0 +1,60 @@
+import com.derman.model.Doctor;
+import com.derman.model.Polyclinic;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class DoctorTest {
+
+    private Doctor doctor;
+
+    @BeforeEach
+    void beforeEachTest(){
+        Polyclinic polyclinic = new Polyclinic(10,"Göz Hastalıkları");
+        doctor = new Doctor(12,"doctor1","ahmet","can","05551233445","123456","doktor",polyclinic);
+    }
+
+    @Test
+    void testDoctorId(){
+        Assertions.assertEquals(12,doctor.getId());
+    }
+
+    @Test
+    void testDoctorName(){
+        Assertions.assertEquals("ahmet",doctor.getName());
+    }
+
+    @Test
+    void testDoctorSurname(){
+        Assertions.assertEquals("can",doctor.getSurname());
+    }
+
+    @Test
+    void testDoctorUsername(){
+        Assertions.assertEquals("doctor1",doctor.getUsername());
+    }
+
+    @Test
+    void testDoctorPhone(){
+        Assertions.assertEquals("05551233445",doctor.getPhone());
+    }
+
+    @Test
+    void testDoctorPassword(){
+        // burada bilerek yanlış girdim
+        Assertions.assertEquals("123456",doctor.getPassword());
+    }
+
+    @Test
+    void testDoctorRole(){
+        // burada bilerek yanlış girdim
+        Assertions.assertEquals("doktor",doctor.getRole());
+    }
+
+    @Test
+    void testDoctorPolyclinicName(){
+        Assertions.assertEquals("Göz Hastalıkları",doctor.getPolyclinic().getName());
+    }
+
+
+}
